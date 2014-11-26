ee8fe2852f89612c41c2ee66277b1f88ba0b9e28
crot4lus <glenn@glenntaylor.co.uk>
2014-11-26 11:47:20 +0000

    Create simpleAccordion.js

---
+++ simpleAccordion.js
@@ -0,0 +1,44 @@
+/* globals $ */
+/* jshint browser: true, devel: true */
+$.fn.simpleAccordion = function (options) {
+	'use strict';
+	var settings = $.extend({ 'customClass': '', 'closeAll': false }, options);
+
+	return this.each(function () {
+
+		var $accordion = $(this);
+
+		$accordion.click(function() {
+
+			var parent = $accordion.parent();
+			var all_items = parent.siblings();
+			var content = $accordion.siblings('.content');
+			var offset = parent.offset().top;
+
+			if(settings.closeAll){
+				$.each(all_items, function(){
+					$(this).removeClass('open');
+					$('.content', $(this)).slideUp();
+				});
+
+				parent.addClass('open');
+				content.slideDown(300);
+			}else{
+				if(parent.hasClass('open')){
+					parent.removeClass('open');
+					content.animate({ opacity: 0 }, 300, 'swing', function(){
+						content.slideUp(300);
+					});
+				} else {
+					content.css({ opacity: 0 });
+					parent.addClass('open');
+					content.slideDown(300, function(){
+						content.animate({ opacity: 1 }, 300);
+					});
+				}
+			}
+
+		});
+
+	});
+};
